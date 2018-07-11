fc736cdff1a9f2b61d2ce095e65586db08e5d9a5
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-07-11 15:38:00 +0200

    Remove unnecessary code

--- src/layers/gee-layer/gee-layer-leaflet.js
+++ src/layers/gee-layer/gee-layer-leaflet.js
@@ -1,7 +1,5 @@
 const GEELayer = (layerModel) => {
-  const body = layerModel.get('body');
   const tileUrl = `https://api.resourcewatch.org/v1/layer/${layerModel.id}/tile/gee/{z}/{x}/{y}`;
-  console.log(layerModel, body)
   const layer = L.tileLayer(tileUrl, layerModel.get('body'));
 
   return new Promise((resolve) => {
