ecbc5d2cd996a1cfe180f95dfbae102cf008a235
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-07-11 17:00:03 +0200

    LOCA & NEXGDDP fixes

--- src/layers/nexgddp-layer/nexgddp-layer-leaflet.js
+++ src/layers/nexgddp-layer/nexgddp-layer-leaflet.js
@@ -1,11 +1,11 @@
 import Promise from 'bluebird';
 
 const NEXGDDPLayer = (layerModel) => {
-  const { id, body, period } = layerModel;
-  const year = (period || {}).value || '1971';
+  const { id, layerConfig } = layerModel;
+  const year = (layerConfig.period || {}).value || '1971';
   const tileUrl = `https://api.resourcewatch.org/v1/layer/${id}/tile/nexgddp/{z}/{x}/{y}?year=${year}`;
 
-  const layer = L.tileLayer(tileUrl, body);
+  const layer = L.tileLayer(tileUrl, layerConfig.body);
 
   return new Promise((resolve) => {
     resolve(layer);
