2a55914e221f9c3a6b1f60e57ce22eb0137ecf14
David Inga <david.inga@vizzuality.com>
2018-08-08 12:59:40 +0200

    using commonjs instead esm and fixed watch command

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,52 @@
 import nodeResolvePlugin from 'rollup-plugin-node-resolve';
 import babelPlugin from 'rollup-plugin-babel';
+import commonjsPlugin from 'rollup-plugin-babel';
 
 export default [{
   input: 'src/index.js',
   output: {
     file: 'dist/index.js',
-    format: 'esm'
+    format: 'cjs',
+    exports: 'named',
   },
+  external: id => id === 'bluebird' || /lodash/.test(id),
   plugins: [
-    nodeResolvePlugin({
-      modulesOnly: true
-    }),
     babelPlugin({
       exclude: 'node_modules/**',
-      plugins: ['external-helpers']
-    })
+      plugins: ['external-helpers'],
+      runtimeHelpers: true
+    }),
+    nodeResolvePlugin({
+      modulesOnly: true,
+      jsnext: true,
+      main: true,
+      browser: true
+    }),
+    commonjsPlugin()
   ]
 }, {
   input: 'src/react/index.js',
   output: {
     file: 'dist/react/index.js',
-    format: 'esm'
+    format: 'cjs',
+    exports: 'named'
   },
+  external: id => id === 'bluebird' ||
+    id === 'react' ||
+    id === 'prop-types' ||
+    /lodash/.test(id),
   plugins: [
-    nodeResolvePlugin({
-      modulesOnly: true
-    }),
     babelPlugin({
       exclude: 'node_modules/**',
-      plugins: ['external-helpers']
-    })
+      plugins: ['external-helpers'],
+      runtimeHelpers: true
+    }),
+    nodeResolvePlugin({
+      modulesOnly: true,
+      jsnext: true,
+      main: true,
+      browser: true
+    }),
+    commonjsPlugin()
   ]
 }];
