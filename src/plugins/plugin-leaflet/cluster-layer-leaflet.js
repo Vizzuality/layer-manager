483a48cf910d75dca7652eace7e2a8fb2ce1bc7f
Ed Brett <e.j.a.brett@gmail.com>
2018-11-02 17:55:18 +0100

    add interaction events for cluster layer

--- src/plugins/plugin-leaflet/cluster-layer-leaflet.js
+++ src/plugins/plugin-leaflet/cluster-layer-leaflet.js
@@ -3,59 +3,91 @@ import Supercluster from 'supercluster';
 
 const { L } = typeof window !== 'undefined' ? window : {};
 
+const defaultSizes = {
+  50: 25,
+  100: 30,
+  1000: 40
+};
+
 const ClusterLayer = L && L.GeoJSON.extend({
-  options: {
-    pointToLayer: (feature, latlng) => {
-      const isCluster = feature.properties && feature.properties.cluster;
-      if (!isCluster) return L.marker(latlng, {
-
-      });
-      const count = feature.properties.point_count;
-      let size = 25;
-      if (count > 100) size = 30;
-      if (count > 1000) size = 40;
-      const icon = L.divIcon({
-        html: `<div style="width: 100%; height: 100%; border-radius: 50%; display: flex; align-items: center; justify-content: center; background-color: orange; color: white; font-size: 14px;">${feature.properties.point_count_abbreviated}</div>`,
-        iconSize: L.point(size,size),
-        className: 'c-cluster-icon'
-      });
-
-      return L.marker(latlng, {
-        icon
-      }).on('click', () => {
-        const { geometry, properties } = feature;
-        if (isCluster) {
-          const center = geometry.coordinates;
-          const zoom = this.supercluster.getClusterExpansionZoom(properties.cluster_id);
-
-          this.map.setCenter(center);
-          this.map.setZoom(zoom);
-        }
-      });
-    }
-  },
-
-  initialize(layerModel, options = {}) {
+  initialize(layerModel) {
+    const self = this;
     L.GeoJSON.prototype.initialize.call(this, []);
-    L.Util.setOptions(this, options);
+    const { layerConfig, events, decodeClusters } = layerModel;
+
+    L.Util.setOptions(this, {
+      // converts feature to icon
+      pointToLayer(feature, latlng) {
+        const isCluster = feature.properties && feature.properties.cluster;
+
+        // if cluster return normal icon
+        if (!isCluster) {
+          // see documentation for icon config https://leafletjs.com/reference-1.3.4.html#icon
+          const icon = L.icon(layerModel.layerConfig.icon);
+          return L.marker(latlng, { icon });
+        }
+
+        const count = feature.properties.point_count;
+        const { html, sizes = defaultSizes } = layerModel.layerConfig || {};
+
+        let iconSize = null;
+        if (typeof sizes === 'function') {
+          iconSize = () => sizes(count)
+        } else {
+          const sizeKey = Object.keys(sizes).find(o => count <= parseInt(o, 10));
+          const size = sizes[sizeKey];
+          iconSize = L.point(size, size);
+        }
+
+        // see documentation for icon config https://leafletjs.com/reference-1.3.4.html#divicon
+        const icon = L.divIcon({
+          iconSize,
+          html: html && typeof html === 'function' ?
+            html(feature) :
+            `<div style="width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; background-color: orange; color: white; font-size: 14px;">${feature.properties.point_count_abbreviated}</div>`,
+          ...layerModel.layerConfig.clusterIcon
+        });
+        return L.marker(latlng, {
+          icon
+        })
+      },
+
+      // parses each feature before adding to the map
+      onEachFeature(feature, layer) {
+        if (feature.properties && feature.properties.cluster) {
+          layer.on({
+            click: () => self.setMapView(feature)
+          });
+        } else if (events) {
+          const parsedEvents = Object.keys(events).reduce((obj, event) => ({
+            ...obj,
+            [event]: e => events[event]({ ...e, data: feature.properties })
+          }), {});
+          layer.on(parsedEvents);
+        }
+      }
+    });
+
+    // https://github.com/mapbox/supercluster options available here
+    const { clusterConfig } = layerConfig.body || {};
     this.supercluster = Supercluster({
       radius: 80,
-      maxZoom: 16
+      maxZoom: 16,
+      ...clusterConfig
     });
 
     fetchData(layerModel)
       .then(response => {
-        const features = response.data.map(d => ({
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [d.attributes.lng, d.attributes.lat]
-          }
-        }));
-
+        const features = decodeClusters(response);
         this.supercluster.load(features);
         this.update();
       })
+  },
+
+  setMapView(feature) {
+    const center = feature.geometry.coordinates;
+    const zoom = this.supercluster.getClusterExpansionZoom(feature.properties.cluster_id);
+    this._map.setView(center.reverse(), zoom);
   },
 
   onAdd(map) {
