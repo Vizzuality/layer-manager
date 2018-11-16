9d93992ce0d0b99f3bae36bce1def2603db86abd
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2018-11-16 14:00:25 +0100

    Adds params parser to layer model on create

--- src/layer-model.js
+++ src/layer-model.js
@@ -1,12 +1,28 @@
 import isEqual from 'lodash/isEqual';
 
+// decodes
+import canvasDecodes from './canvas-decodes';
+
 class LayerModel {
   opacity = 1;
 
   visibility = true;
 
   constructor(layerSpec = {}) {
-    Object.assign(this, layerSpec, { changedAttributes: {} });
+    // parse params before setting model's properties
+    Object.assign(this, {
+      ...layerSpec,
+      ...{
+        ...LayerModel.parse(layerSpec),
+        // compatibility
+        ...layerSpec.params && { params: layerSpec.params },
+        ...layerSpec.sqlParams && { sqlParams: layerSpec.sqlParams },
+        ...layerSpec.decodeParams && { decodeParams: layerSpec.decodeParams },
+        // adds decodeFunction if the layerSpec contains decode_config params (it is a canvas layer)
+        ...Object.prototype.hasOwnProperty.call(layerSpec.layerConfig, 'decode_config') &&
+          { decodeFunction: canvasDecodes[layerSpec.id] }
+      }
+    }, { changedAttributes: {} });
   }
 
   get(key) {
@@ -20,7 +36,9 @@ class LayerModel {
 
   update(layerSpec) {
     const prevData = { ...this };
-    const nextData = { ...layerSpec };
+    const nextData = {
+      ...layerSpec
+    };
 
     // reseting changedAttributes for every update
     this.set('changedAttributes', {});
@@ -31,6 +49,42 @@ class LayerModel {
         this.set(k, nextData[k]);
       }
     });
+  }
+
+  static parse(layerSpec = {}) {
+    const {
+      params_config: paramsConfig,
+      sqlParams_config: sqlParamsConfig,
+      decode_config: decodeConfig,
+      body
+    } = layerSpec.layerConfig;
+    const { url } = body;
+    let params = {};
+    const sqlParams = {};
+    const decodeParams = {};
+
+    (paramsConfig || []).forEach(_param => {
+      if (_param.key && _param.default) params[_param.key] = _param.default;
+    });
+
+    params = {
+      ...params,
+      url
+    };
+
+    (sqlParamsConfig || []).forEach(_param => {
+      if (_param.key && _param.default) sqlParams[_param.key] = _param.default;
+    });
+
+    (decodeConfig || []).forEach(_param => {
+      if (_param.key && _param.default) decodeParams[_param.key] = _param.default;
+    });
+
+    return {
+      params,
+      sqlParams,
+      decodeParams
+    };
   }
 }
 
