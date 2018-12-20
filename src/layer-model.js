6baae97850f4d838a188259abc9c1b47e9a504ca
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2018-12-20 19:00:24 +0100

    Adds canvas decoders

--- src/layer-model.js
+++ src/layer-model.js
@@ -1,12 +1,30 @@
 import isEqual from 'lodash/isEqual';
 
+// decodes
+import canvasDecodes from './canvas-decodes';
+
+
 class LayerModel {
   opacity = 1;
 
   visibility = true;
 
   constructor(layerSpec = {}) {
-    Object.assign(this, layerSpec, { changedAttributes: {} });
+    Object.assign(
+      this,
+      {
+        ...layerSpec,
+        ...!layerSpec.decodeFunction && canvasDecodes[layerSpec.id] &&
+        { decodeFunction: canvasDecodes[layerSpec.id] },
+        changedAttributes: {}
+      }
+    );
+
+    // if the model has decodeParams but there's no decoder available, will show a warning.
+    if (Object.prototype.hasOwnProperty.call(this, 'decodeParams') &&
+      (!layerSpec.decodeFunction)  && !this.decodeFunction) {
+        console.warn(`${layerSpec.id}: canvas decoder not found.`)
+      }
   }
 
   get(key) {
