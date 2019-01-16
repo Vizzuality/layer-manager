95b345feefb53ed675482e9c87383af435c6df39
Ed Brett <e.j.a.brett@gmail.com>
2019-01-16 16:31:29 +0100

    initialize cancel token after instance of cancel

--- src/services/cluster-service.js
+++ src/services/cluster-service.js
@@ -2,16 +2,17 @@ import { CancelToken } from 'axios';
 import { get } from 'lib/request';
 
 export const fetchData = (layerModel) => {
-  const source = CancelToken.source();
-  const { layerConfig } = layerModel;
-  const { layerRequest } = layerModel;
+  const { layerConfig, layerRequest } = layerModel;
   const { url } = layerConfig.body;
 
-  if (layerRequest && layerRequest instanceof Promise) {
-    source.cancel('Operation canceled by the user.');
+  if (layerRequest) {
+    layerRequest.cancel('Operation canceled by the user.');
   }
 
-  const newLayerRequest = get(url, { cancelToken: source.token }).then((res) => {
+  const layerRequestSource = CancelToken.source();
+  layerModel.set('layerRequest', layerRequestSource);
+
+  const newLayerRequest = get(url, { cancelToken: layerRequestSource.token }).then((res) => {
     if (res.status > 400) {
       console.error(res);
       return false;
