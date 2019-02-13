f3710c9a58876fc19c26d939e1161b13c390320d
Ed Brett <e.j.a.brett@gmail.com>
2019-02-13 17:01:45 +0100

    update to raster layer

--- src/plugins/plugin-mapbox-gl/index.js
+++ src/plugins/plugin-mapbox-gl/index.js
@@ -9,7 +9,7 @@ class PluginMapboxGL {
   }
 
   method = {
-    leaflet: geoJsonLayer,
+    leaflet: rasterLayer,
     gee: rasterLayer,
     cartodb: vectorLayer,
     mapbox: vectorLayer,
@@ -136,27 +136,6 @@ class PluginMapboxGL {
   }
 
   setEvents() {
-    // inspect a cluster on click
-    this.map.on('click', 'clusters', (e) => {
-      console.log('zzomigf');
-      const features = this.map.queryRenderedFeatures(e.point, { layers: ['clusters'] });
-      const clusterId = features[0].properties.cluster_id;
-      this.map.getSource('d7b12b17-9ed4-43ab-b8e4-efa2668c47f8').getClusterExpansionZoom(clusterId, (err, zoom) => {
-        if (err) return;
-        this.map.easeTo({
-          center: features[0].geometry.coordinates,
-          zoom
-        });
-      });
-    });
-
-    this.map.on('mouseenter', 'clusters', () => {
-      this.map.getCanvas().style.cursor = 'pointer';
-    });
-
-    this.map.on('mouseleave', 'clusters', () => {
-      this.map.getCanvas().style.cursor = '';
-    });
   }
 
   setDecodeParams(layerModel) {
