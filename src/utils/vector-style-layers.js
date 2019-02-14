6803c376354ca1dceb9dc8436497271d6431d0cf
Ed Brett <e.j.a.brett@gmail.com>
2019-02-14 13:17:05 +0100

    parse layer params through replace

--- src/utils/vector-style-layers.js
+++ src/utils/vector-style-layers.js
@@ -1,3 +1,5 @@
+import { replace } from './query';
+
 /**
  * Parsed vector style layers that follow the mapbox style layer spec
  * and returns mountable layers with ids and opacity applied
@@ -5,30 +7,34 @@
  * @param {Object} layerModel
  */
 export const getVectorStyleLayers = (vectorLayers, layerModel) => {
-  const { id } = layerModel;
+  const { id, params, sqlParams } = layerModel;
+  if (vectorLayers && vectorLayers.length) {
+    const vectorLayersParsed = JSON.parse(replace(JSON.stringify(vectorLayers), params, sqlParams));
+    return vectorLayersParsed && vectorLayersParsed.map((l, i) => {
+      const layerOpacity = l.opacity ? layerModel.opacity * l.opacity : layerModel.opacity;
+      let opacityPaintTypes = [l.type];
+      if (l.type === 'symbol') opacityPaintTypes = ['icon', 'text'];
+      if (l.type === 'circle') opacityPaintTypes = ['circle', 'circle-stroke'];
+      const opacityPaintStyles = opacityPaintTypes.reduce((obj, type) => ({
+        ...obj,
+        [`${type}-opacity`]: layerOpacity
+      }), {});
 
-  return vectorLayers.map((l, i) => {
-    const layerOpacity = l.opacity ? layerModel.opacity * l.opacity : layerModel.opacity;
-    let opacityPaintTypes = [l.type];
-    if (l.type === 'symbol') opacityPaintTypes = ['icon', 'text'];
-    if (l.type === 'circle') opacityPaintTypes = ['circle', 'circle-stroke'];
-    const opacityPaintStyles = opacityPaintTypes.reduce((obj, type) => ({
-      ...obj,
-      [`${type}-opacity`]: layerOpacity
-    }), {});
-
-    return {
-      ...l,
-      id: `${id}-${l.type}-${i}`,
-      source: id,
-      ...l.paint && {
-        paint: {
-          ...l.paint,
-          ...opacityPaintStyles
+      return {
+        ...l,
+        id: `${id}-${l.type}-${i}`,
+        source: id,
+        ...l.paint && {
+          paint: {
+            ...l.paint,
+            ...opacityPaintStyles
+          }
         }
-      }
-    };
-  });
+      };
+    });
+  }
+
+  return false;
 };
 
 export default { getVectorStyleLayers };
