a902d3a2b14e7a95b767b14e51ea4e3047ab13d1
Ed Brett <e.j.a.brett@gmail.com>
2019-07-22 18:19:13 +0200

    upgrade mapbox layer and bitmap layer to latest version

--- src/plugins/plugin-mapbox-gl/custom-layers/bitmap-layer/index.js
+++ src/plugins/plugin-mapbox-gl/custom-layers/bitmap-layer/index.js
@@ -22,23 +22,32 @@
 import GL from '@luma.gl/constants';
 import { Layer } from '@deck.gl/core';
 import { Model, Geometry, Texture2D, fp64 } from 'luma.gl';
-import { loadImage } from '@loaders.gl/core';
+import { loadImage } from '@loaders.gl/images';
 import vs from './bitmap-layer-vertex';
 import shaderTemplate from './bitmap-layer-fragment';
 
+
 const { fp64LowPart } = fp64;
+
+const DEFAULT_TEXTURE_PARAMETERS = {
+  [GL.TEXTURE_MIN_FILTER]: GL.LINEAR_MIPMAP_LINEAR,
+  // GL.LINEAR is the default value but explicitly set it here
+  [GL.TEXTURE_MAG_FILTER]: GL.LINEAR,
+  [GL.TEXTURE_WRAP_S]: GL.CLAMP_TO_EDGE,
+  [GL.TEXTURE_WRAP_T]: GL.CLAMP_TO_EDGE
+};
 
 const defaultProps = {
   image: null,
-  bitmapBounds: { type: 'array', value: [1, 0, 0, 1], compare: true },
+  bounds: {type: 'array', value: [1, 0, 0, 1], compare: true},
   fp64: false,
 
-  desaturate: { type: 'number', min: 0, max: 1, value: 0 },
+  desaturate: {type: 'number', min: 0, max: 1, value: 0},
   // More context: because of the blending mode we're using for ground imagery,
   // alpha is not effective when blending the bitmap layers with the base map.
   // Instead we need to manually dim/blend rgb values with a background color.
-  transparentColor: { type: 'color', value: [0, 0, 0, 0] },
-  tintColor: { type: 'color', value: [255, 255, 255] }
+  transparentColor: {type: 'color', value: [0, 0, 0, 0]},
+  tintColor: {type: 'color', value: [255, 255, 255]}
 };
 
 /*
@@ -50,7 +59,6 @@ const defaultProps = {
 export default class BitmapLayer extends Layer {
   getShaders() {
     const projectModule = this.use64bitProjection() ? 'project64' : 'project32';
-
     const fs = shaderTemplate
       .replace('{decodeParams}', Object.keys(this.props.decodeParams).map(p => `uniform float ${p};`).join(' '))
       .replace('{decodeFunction}', this.props.decodeFunction || '');
@@ -60,57 +68,61 @@ export default class BitmapLayer extends Layer {
 
   initializeState() {
     const attributeManager = this.getAttributeManager();
-    /*
-      -1,1  ---  1,1
-        |         |
-      -1,-1 --- 1,-1
-     */
-    const positions = [-1, -1, 0, -1, 1, 0, 1, 1, 0, 1, -1, 0];
 
     attributeManager.add({
       positions: {
         size: 3,
         update: this.calculatePositions,
-        value: new Float32Array(positions)
+        value: new Float32Array(12)
       },
       positions64xyLow: {
         size: 3,
         update: this.calculatePositions64xyLow,
-        value: new Float32Array(positions)
+        value: new Float32Array(12)
       }
     });
+
+    this.setState({numInstances: 4}); // 4 corners
   }
 
-  updateState({ props, oldProps }) {
+  updateState({props, oldProps, changeFlags}) {
     // setup model first
     if (props.fp64 !== oldProps.fp64) {
-      const { gl } = this.context;
+      const {gl} = this.context;
       if (this.state.model) {
         this.state.model.delete();
       }
-      this.setState({ model: this._getModel(gl) });
+      this.setState({model: this._getModel(gl)});
       this.getAttributeManager().invalidateAll();
     }
 
     if (props.image !== oldProps.image) {
-      this.loadTexture();
+      this.loadTexture(props.image);
     }
 
     const attributeManager = this.getAttributeManager();
 
-    if (props.bitmapBounds !== oldProps.bitmapBounds) {
+    if (props.bounds !== oldProps.bounds) {
       this.setState({
-        positions: this._getPositionsFromBounds(props.bitmapBounds)
+        positions: this._getPositionsFromBounds(props.bounds)
       });
       attributeManager.invalidate('positions');
       attributeManager.invalidate('positions64xyLow');
     }
   }
 
-  _getPositionsFromBounds(bitmapBounds) {
+  finalizeState() {
+    super.finalizeState();
+
+    if (this.state.bitmapTexture) {
+      this.state.bitmapTexture.delete();
+    }
+  }
+
+  _getPositionsFromBounds(bounds) {
     const positions = new Array(12);
-    // bitmapBounds as [minX, minY, maxX, maxY]
-    if (Number.isFinite(bitmapBounds[0])) {
+    // bounds as [minX, minY, maxX, maxY]
+    if (Number.isFinite(bounds[0])) {
       /*
         (minX0, maxY3) ---- (maxX2, maxY3)
                |                  |
@@ -118,27 +130,27 @@ export default class BitmapLayer extends Layer {
                |                  |
         (minX0, minY1) ---- (maxX2, minY1)
      */
-      positions[0] = bitmapBounds[0];
-      positions[1] = bitmapBounds[1];
+      positions[0] = bounds[0];
+      positions[1] = bounds[1];
       positions[2] = 0;
 
-      positions[3] = bitmapBounds[0];
-      positions[4] = bitmapBounds[3];
+      positions[3] = bounds[0];
+      positions[4] = bounds[3];
       positions[5] = 0;
 
-      positions[6] = bitmapBounds[2];
-      positions[7] = bitmapBounds[3];
+      positions[6] = bounds[2];
+      positions[7] = bounds[3];
       positions[8] = 0;
 
-      positions[9] = bitmapBounds[2];
-      positions[10] = bitmapBounds[1];
+      positions[9] = bounds[2];
+      positions[10] = bounds[1];
       positions[11] = 0;
     } else {
       // [[minX, minY], [minX, maxY], [maxX, maxY], [maxX, minY]]
-      for (let i = 0; i < bitmapBounds.length; i++) {
-        positions[i * 3 + 0] = bitmapBounds[i][0];
-        positions[i * 3 + 1] = bitmapBounds[i][1];
-        positions[i * 3 + 2] = bitmapBounds[i][2] || 0;
+      for (let i = 0; i < bounds.length; i++) {
+        positions[i * 3 + 0] = bounds[i][0];
+        positions[i * 3 + 1] = bounds[i][1];
+        positions[i * 3 + 2] = bounds[i][2] || 0;
       }
     }
 
@@ -176,67 +188,58 @@ export default class BitmapLayer extends Layer {
     const { bitmapTexture, model } = this.state;
     const { desaturate, transparentColor, tintColor, zoom, decodeParams, opacity } = this.props;
 
-    // TODO fix zFighting
+    // // TODO fix zFighting
     // Render the image
     if (bitmapTexture && model) {
-      model.render(
-        Object.assign({}, uniforms, {
-          bitmapTexture,
-          desaturate,
-          transparentColor,
-          tintColor,
-          zoom,
-          ...decodeParams,
-          opacity
-        })
-      );
+      model
+        .setUniforms(
+          Object.assign({}, uniforms, {
+            bitmapTexture,
+            desaturate,
+            transparentColor,
+            tintColor,
+            zoom,
+            ...decodeParams,
+            opacity
+          })
+        )
+        .draw();
     }
   }
 
-  loadTexture() {
-    const { gl } = this.context;
-    const { image } = this.props;
-
-    const textureOptions = {
-      parameters: {
-        [GL.TEXTURE_MIN_FILTER]: GL.NEAREST,
-        [GL.TEXTURE_MAG_FILTER]: GL.NEAREST,
-        [GL.TEXTURE_WRAP_S]: GL.CLAMP_TO_EDGE,
-        [GL.TEXTURE_WRAP_T]: GL.CLAMP_TO_EDGE,
-      },
-      mipmaps: false
-    };
-
+  loadTexture(image) {
     if (typeof image === 'string') {
-      loadImage(image).then((data) => {
-        this.setState({
-          bitmapTexture: new Texture2D(gl, {
-            data,
-            ...textureOptions
-          })
-        });
-      })
-      .catch(e => {
-        console.warn(e);
-      });
-    } else if (image instanceof Texture2D) {
-      this.setState({ bitmapTexture: image });
+      image = loadImage(image);
+    }
+    if (image instanceof Promise) {
+      image.then(data => this.loadTexture(data));
+      return;
+    }
+
+    const {gl} = this.context;
+
+    if (this.state.bitmapTexture) {
+      this.state.bitmapTexture.delete();
+    }
+
+    if (image instanceof Texture2D) {
+      this.setState({bitmapTexture: image});
     } else if (
       // browser object
-      image instanceof Image
-      || image instanceof HTMLCanvasElement
+      image instanceof Image ||
+      image instanceof HTMLCanvasElement
     ) {
       this.setState({
         bitmapTexture: new Texture2D(gl, {
           data: image,
-          ...textureOptions
+          parameters: DEFAULT_TEXTURE_PARAMETERS
         })
       });
     }
   }
 
-  calculatePositions({ value }) {
-    const { positions } = this.state;
+  calculatePositions({value}) {
+    const {positions} = this.state;
     value.set(positions);
   }
 
@@ -249,7 +252,7 @@ export default class BitmapLayer extends Layer {
       return;
     }
 
-    const { value } = attribute;
+    const {value} = attribute;
     value.set(this.state.positions.map(fp64LowPart));
   }
 }
