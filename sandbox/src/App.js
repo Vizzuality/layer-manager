03745a0fc2e37ae3f0263660e954eecc08abccad
Daniel Fernández Rico <deferrico@gmail.com>
2019-09-06 11:21:25 +0200

    Imported LM in sandbox

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import Map from 'components/map';
+// import { LayerManager, Layer } from 'layer-manager/dist/components';
+import { LayerManager, Layer } from '../../src/components';
+// import { PluginMapboxGl } from 'layer-manager'
+import { PluginMapboxGl } from '../../src/plugins/plugin-mapbox-gl'
 
 import './App.scss'
 
 function App() {
+  const layers = [];
+
   return (
     <div className="c-app">
       <Map
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/layer-manager/ck06v389m02u61cqq3qqd78hq"
+        mapStyle="mapbox://styles/layer-manager/ck07vfinn01xm1co324q5vcdl"
       >
-        {(map) => {
-          // Layer Manager goes here
-        }}
+        {(map) =>
+          <LayerManager
+            map={map}
+            plugin={PluginMapboxGl}
+          >
+            {layers.map(layer => (
+              <Layer
+                key={layer.id}
+                {...layer}
+              />
+            ))}
+          </LayerManager>
+        }
       </Map>
     </div>
   );
