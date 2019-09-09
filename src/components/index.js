00baaf546296fcb02a6cfcfa2b4a572bc34f4a1d
Miguel Barrenechea Sánchez <miguelbarrenechea@MacBook-Pro.home>
2019-09-09 16:15:09 +0200

    Updates

--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as LayerManager } from './layer-manager';
-export { default as Layer } from './layer';
--- src/plugins/plugin-cesium/tile-layer-cesium.js
+++
@@ -1,8 +0,0 @@
-export default Cesium => layerModel => new Promise((resolve) => {
-  const { layerConfig = {} } = layerModel;
-  const { url } = layerConfig.body;
-  const provider = new Cesium.UrlTemplateImageryProvider({ url });
-  provider.errorEvent.addEventListener(() => false);
-  // don't show warnings
-  resolve(new Cesium.ImageryLayer(provider));
-});
