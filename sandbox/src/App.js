3e09930678a8df4dff02e068b2fd3a396b719213
Miguel Barrenechea Sánchez <miguelbarrenechea@MacBook-Pro.home>
2019-09-11 18:36:57 +0200

    Legend added

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -1,18 +1,53 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Map from 'components/map';
+
+// Layer manager
 import { LayerManager, Layer } from 'layer-manager/dist/components';
 import { PluginMapboxGl } from 'layer-manager'
-import layers from 'layers.json';
 
-import LayerToggle from 'components/map/controls/layer-toggle';
+// Legend
+import {
+  Icons,
+  Legend,
+  LegendListItem,
+  LegendItemTypes,
+  LegendItemToolbar,
+  LegendItemButtonOpacity,
+  LegendItemButtonVisibility,
+  LegendItemButtonRemove
+} from 'vizzuality-components';
+
+// DATA
+import layers from './layers.json';
 
 import './App.scss'
 
 function App() {
-  const [activeLayers, setActiveLayers] = useState(layers.map(l => ({...l, active: true})));
+  const layerGroups = layers.map(l => {
+    const { id } = l;
+
+    return {
+      slug: id,
+      dataset: id,
+      layers: [{
+        active: true,
+        ...l
+      }]
+    }
+  });
+
+  const onChangeOrder = (...props) => { console.log('onChangeOrder', props); }
+  const onChangeVisibility = (...props) => { console.log('onChangeVisibility', props); }
+  const onChangeOpacity = (...props) => { console.log('onChangeOpacity', props); }
+  const onRemoveLayer = (...props) => { console.log('onRemoveLayer', props); }
+
+  console.log(layerGroups);
+
 
   return (
     <div className="c-app">
+      <Icons />
+
       <Map
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         mapStyle="mapbox://styles/layer-manager/ck07vfinn01xm1co324q5vcdl"
@@ -22,7 +57,7 @@ function App() {
             map={map}
             plugin={PluginMapboxGl}
           >
-            {activeLayers.filter(l => l.active).map(layer => (
+            {layers.map(layer => (
               <Layer
                 key={layer.id}
                 {...layer}
@@ -31,7 +66,43 @@ function App() {
           </LayerManager>
         }
       </Map>
-      <LayerToggle layers={activeLayers} setLayers={setActiveLayers} />
+
+      <div className="c-legend">
+        <Legend
+          maxHeight={'65vh'}
+          collapsable={false}
+          onChangeOrder={onChangeOrder}
+        >
+          {layerGroups.map((layerGroup, i) => {
+            return (
+              <LegendListItem
+                index={i}
+                key={layerGroup.slug}
+                layerGroup={layerGroup}
+                toolbar={
+                  <LegendItemToolbar>
+                    <LegendItemButtonOpacity
+                      trackStyle={{
+                        background: '#FFCC00'
+                      }}
+                      handleStyle={{
+                        background: '#FFCC00'
+                      }}
+                    />
+                    <LegendItemButtonVisibility />
+                    <LegendItemButtonRemove />
+                  </LegendItemToolbar>
+                }
+                onChangeVisibility={((l, visibility) => onChangeVisibility(l, visibility, layerGroup.slug))}
+                onChangeOpacity={(l, opacity) => onChangeOpacity(l, opacity, layerGroup.slug)}
+                onRemoveLayer={(l) => { onRemoveLayer(l)}}
+              >
+                <LegendItemTypes />
+              </LegendListItem>
+            )
+          })}
+        </Legend>
+      </div>
     </div>
   );
 }
