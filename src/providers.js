b07102d8b46dd17298f953d7047317108fe7aef3
Miguel Barrenechea Sánchez <miguelbarrenechea@MacBook-Pro.home>
2020-03-13 11:35:02 +0100

    Carto provider: remove unnecessary options attribute

--- src/providers.js
+++ src/providers.js
@@ -8,7 +8,7 @@ export default {
     const layerTpl = JSON.stringify({
       version: '1.3.0',
       stat_tag: 'API',
-      layers: provider.options.layers.map(l => {
+      layers: provider.layers.map(l => {
         if (!!interactivity && interactivity.length) {
           return { ...l, options: { ...l.options, interactivity } };
         }
@@ -20,18 +20,18 @@ export default {
     const apiParams = {
       stat_tag: 'API',
       config: encodeURIComponent(layerTpl),
-      ...(provider.options.api_key && { api_key: provider.options.api_key })
+      ...(provider.api_key && { api_key: provider.api_key })
     };
     const apiParamsString = Object.keys(apiParams)
       .map(k => `${k}=${apiParams[k]}`)
       .join('&');
-    const url = `https://${provider.options.account}.carto.com/api/v1/map?${apiParamsString}`;
+    const url = `https://${provider.account}.carto.com/api/v1/map?${apiParamsString}`;
 
     fetch('get', url, {}, layerModel)
       .then(response => {
         const ext = layerModel.type === 'vector' ? 'mvt' : 'png';
         const tileUrl = `${response.cdn_url.templates.https.url.replace('{s}', 'a')}/${
-          provider.options.account
+          provider.account
         }/api/v1/map/${response.layergroupid}/{z}/{x}/{y}.${ext}`;
 
         return resolve({
