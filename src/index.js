4764f2d6e5e7e1a7fa8171ac4c7bbce09f7cabeb
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2021-01-25 17:54:49 +0100

    monorepo foundation

--- src/index.js
+++
@@ -1,8 +0,0 @@
-export { default } from './layer-manager';
-
-// Plugins
-export { default as PluginMapboxGl } from './plugins/plugin-mapbox-gl';
-
-// Utils
-export { replace, substitution, concatenation } from './utils/query';
-export { fetch } from './utils/request';
---
+++ src/packages/components/react/rollup.config.js
@@ -0,0 +1,41 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import pkg from './package.json';
+
+const globals = {
+  react: 'React',
+  'prop-types': 'PropTypes',
+  axios: 'axios',
+  'lodash/isEmpty': 'isEmpty',
+  'lodash/isEqual': 'isEqual'
+};
+const external = Object.keys(globals);
+const babelOptions = () => ({
+  babelrc: false,
+  presets: [['env', { modules: false }], 'react'],
+  plugins: [
+    'transform-class-properties',
+    'transform-object-rest-spread',
+    'external-helpers',
+    [
+      'module-resolver',
+      {
+        root: ['./src/**'],
+        extensions: ['.js', '.jsx']
+      }
+    ]
+  ]
+});
+
+export default [
+  {
+    input: './src/index.js',
+    output: {
+      file: pkg.module,
+      format: 'es'
+    },
+    external,
+    plugins: [babel(babelOptions()), resolve(), commonjs()]
+  }
+];
