edfba2bbababb1c46dde4db2ddd1368b82d041cc
David Inga <david.inga@vizzuality.com>
2021-05-24 11:36:58 +0200

    removed not needed file

--- packages/utils/index.d.ts
+++
@@ -1,9 +0,0 @@
-export { fetch, replace, concatenation, substitution } from './src';
-
-export type QueryParams = {
-  [key:string]: string;
-}
-
-export type WhereQueryParams = {
-  [key: string]: Record<string, unknown | unknown[]>
-}
