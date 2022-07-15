e6980df9f29a7691aaeb0739bca62901551e0cab
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-07-15 09:19:46 +0200

    Update package version

--- applications/docs/src/stories/playground/apng-layer/prediction.stories.tsx
+++ applications/docs/src/stories/playground/apng-layer/prediction.stories.tsx
@@ -10,6 +10,7 @@ import GL from '@luma.gl/constants';
 import { MapboxLayer } from '@deck.gl/mapbox';
 import { TileLayer } from '@deck.gl/geo-layers';
 import { BitmapLayer } from '@deck.gl/layers';
+import { DecodedLayer } from '@vizzuality/layer-manager-layers-deckgl';
 
 import parseAPNG from 'apng-js';
 
@@ -49,6 +50,10 @@ const Template: Story<LayerProps> = (args: LayerProps) => {
           id: `prediction-animated`,
           type: TileLayer,
           frame,
+          getPolygonOffset: () => {
+            return [0, -50];
+          },
+
           getTileData: (tile) => {
             const { x, y, z, signal } = tile;
             const url = `https://storage.googleapis.com/geo-ai/Redes/Tiles/Menongue/APNGs/Prediction/${z}/${x}/${y}.png`;
@@ -106,6 +111,9 @@ const Template: Story<LayerProps> = (args: LayerProps) => {
                 id: subLayerId,
                 image: FRAME.bitmapData,
                 bounds: [west, south, east, north],
+                getPolygonOffset: () => {
+                  return [0, -50];
+                },
                 textureParameters: {
                   [GL.TEXTURE_MIN_FILTER]: GL.NEAREST,
                   [GL.TEXTURE_MAG_FILTER]: GL.NEAREST,
@@ -122,6 +130,96 @@ const Template: Story<LayerProps> = (args: LayerProps) => {
           minZoom: 10,
           maxZoom: 14,
           extent: bounds.bbox,
+        }
+      ),
+      new MapboxLayer(
+        {
+          id: `deck-loss-raster-decode-animated`,
+          type: TileLayer,
+          data: 'https://storage.googleapis.com/wri-public/Hansen_16/tiles/hansen_world/v1/tc30/{z}/{x}/{y}.png',
+          tileSize: 256,
+          visible: true,
+          opacity: 1,
+          refinementStrategy: 'no-overlap',
+          decodeParams: {
+            startYear: 1988,
+            endYear: 1988 + frame,
+          },
+          getPolygonOffset: () => {
+            return [0, -100];
+          },
+
+          renderSubLayers: (sl) => {
+            const {
+              id: subLayerId,
+              data,
+              tile,
+              visible,
+              opacity: _opacity,
+              decodeParams: _decodeParams,
+            } = sl;
+
+            const {
+              z,
+              bbox: {
+                west, south, east, north,
+              },
+            } = tile;
+
+            if (data) {
+              return new DecodedLayer({
+                id: subLayerId,
+                image: data,
+                bounds: [west, south, east, north],
+                getPolygonOffset: () => {
+                  return [0, -100];
+                },
+                textureParameters: {
+                  [GL.TEXTURE_MIN_FILTER]: GL.NEAREST,
+                  [GL.TEXTURE_MAG_FILTER]: GL.NEAREST,
+                  [GL.TEXTURE_WRAP_S]: GL.CLAMP_TO_EDGE,
+                  [GL.TEXTURE_WRAP_T]: GL.CLAMP_TO_EDGE,
+                },
+                zoom: z,
+                visible,
+                opacity: _opacity,
+                decodeParams: _decodeParams,
+                decodeFunction: `
+                  // values for creating power scale, domain (input), and range (output)
+                  float domainMin = 0.;
+                  float domainMax = 255.;
+                  float rangeMin = 0.;
+                  float rangeMax = 255.;
+
+                  float exponent = zoom < 13. ? 0.3 + (zoom - 3.) / 20. : 1.;
+                  float intensity = color.r * 255.;
+
+                  // get the min, max, and current values on the power scale
+                  float minPow = pow(domainMin, exponent - domainMin);
+                  float maxPow = pow(domainMax, exponent);
+                  float currentPow = pow(intensity, exponent);
+
+                  // get intensity value mapped to range
+                  float scaleIntensity = ((currentPow - minPow) / (maxPow - minPow) * (rangeMax - rangeMin)) + rangeMin;
+                  // a value between 0 and 255
+                  alpha = zoom < 13. ? scaleIntensity / 255. : color.g;
+
+                  float year = 2000.0 + (color.b * 255.);
+                  // map to years
+                  if (year >= startYear && year <= endYear && year >= 2001.) {
+                    color.r = 220. / 255.;
+                    color.g = (72. - zoom + 102. - 3. * scaleIntensity / zoom) / 255.;
+                    color.b = (33. - zoom + 153. - intensity / zoom) / 255.;
+                  } else {
+                    alpha = 0.;
+                  }
+                `
+              });
+            }
+            return null;
+          },
+          minZoom: 3,
+          maxZoom: 12,
         }
       )
     ]
